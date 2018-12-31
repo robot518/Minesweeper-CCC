fbe36426a5db1fa224e1b5287215aa167ce12025
smile <262000730@qq.com>
2019-01-01 02:06:53 +0800

    Add files via upload

---
+++ MineServer/src/websocketx/Redis.java
@@ -0,0 +1,165 @@
+package websocketx;
+
+import redis.clients.jedis.*;
+
+import java.util.Set;
+
+public class Redis {
+    private JedisPool pool;
+    private static Redis redis;
+    private String sKeyScore = "score";
+    private String sKeyStep = "step";
+
+    public static Redis getInstance(){
+        if (redis == null){
+            redis = new Redis();
+        }
+        return redis;
+    }
+
+    private JedisPool getPool(){
+        if (pool == null){
+            pool = new JedisPool(new JedisPoolConfig(), "localhost", Protocol.DEFAULT_PORT, Protocol.DEFAULT_TIMEOUT, "1314");
+        }
+        return pool;
+    }
+
+    public boolean setName(String sName, String sPass){
+        Jedis jedis = null;
+        try {
+            jedis = getPool().getResource();
+            if (jedis.get(sName) == null) {
+                jedis.set(sName, sPass);
+                return true;
+            }
+            return false;
+        } finally {
+            if (jedis != null) {
+                jedis.close();
+            }
+        }
+    }
+
+    public String getName(String sName){
+        Jedis jedis = null;
+        try {
+            jedis = getPool().getResource();
+            return jedis.get(sName);
+        } finally {
+            if (jedis != null) {
+                jedis.close();
+            }
+        }
+    }
+
+    public String getStep(String sIdx, String sName){
+        Jedis jedis = null;
+        try {
+            jedis = getPool().getResource();
+            return jedis.hget(sKeyStep+sIdx, sName);
+        } finally {
+            if (jedis != null) {
+                jedis.close();
+            }
+        }
+    }
+
+    public void addStep(String sIdx, String sName, String sStep){
+        Jedis jedis = null;
+        try {
+            jedis = getPool().getResource();
+            jedis.hset(sKeyStep+sIdx, sName, sStep);
+        } finally {
+            if (jedis != null) {
+                jedis.close();
+            }
+        }
+    }
+
+    public void addScore(String sIdx, String sName, float iScore){
+        Jedis jedis = null;
+        try {
+            jedis = getPool().getResource();
+            /// ... do stuff here ... for example
+            Double iScorePre = jedis.zscore(sKeyScore + sIdx, sName);
+            if (iScorePre == null || iScore < iScorePre)
+                jedis.zadd(sKeyScore + sIdx, iScore, sName);
+        } finally {
+            // You have to close jedis object. If you don't close then
+            // it doesn't release back to pool and you can't get a new
+            // resource from pool.
+            if (jedis != null) {
+                jedis.close();
+            }
+        }
+    }
+
+    public String getStrScore(String sName){
+        if (sName == null || sName == "")
+            return "";
+        Jedis jedis = null;
+        try {
+            jedis = getPool().getResource();
+            String str = "";
+            for (int i = 0; i < 3; i++){
+                String sKeyTemp = sKeyScore+i;
+                Long iRank = jedis.zrank(sKeyTemp, sName);
+                if (iRank != null) {
+                    Double iScore = jedis.zscore(sKeyTemp, sName);
+                    str += (iRank+1) + "," + iScore + ",";
+                    int iRecommend = 0;
+                    if (iRank == 0)
+                        iRecommend = 1;
+                    else if (iRank <= 10)
+                        iRecommend = (int) (iRank - 1);
+                    else if (iRank <= 100)
+                        iRecommend = (int) (iRank - 5);
+                    else if (iRank <= 200)
+                        iRecommend = (int) (iRank - 10);
+                    else if (iRank <= 500)
+                        iRecommend = (int) (iRank - 50);
+                    else if (iRank <= 1000)
+                        iRecommend = (int) (iRank - 100);
+                    else {
+                        Long iCount = jedis.zcard(sKeyTemp);
+                        iRecommend = iCount >= 200 ? 200 : (int) (iCount - 0);
+                    }
+                    Set<Tuple> st = jedis.zrangeWithScores(sKeyTemp, iRecommend, iRecommend);
+                    str += (iRecommend+1) + ",";
+                    str += st.toString().replace("], [", ",").replace("[", "").replace("]", "");
+                }
+                if (i != 3)
+                    str += "|";
+            }
+//            System.out.println(str);
+            return str;
+        } finally {
+            if (jedis != null) {
+                jedis.close();
+            }
+        }
+    }
+
+    public String getStrRank(String sIdx){
+        Jedis jedis = null;
+        try {
+            jedis = getPool().getResource();
+            Set<Tuple> stItems = jedis.zrangeWithScores(sKeyScore + sIdx, 0, 9);
+            String str = stItems.toString();
+            if (str.length() == 2)
+                return "";
+            str = str.replace("], [", "|");
+            str = str.replace("[", "");
+            str = str.replace("]", "");
+            return str;
+        } finally {
+            if (jedis != null) {
+                jedis.close();
+            }
+        }
+    }
+
+    public void close(){
+        getPool().close();
+    }
+}
---
+++ MineServer/src/websocketx/WebSocketFrameHandler.java
@@ -0,0 +1,130 @@
+/*
+ * Copyright 2012 The Netty Project
+ *
+ * The Netty Project licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+package websocketx;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.handler.codec.http.websocketx.TextWebSocketFrame;
+import io.netty.handler.codec.http.websocketx.WebSocketFrame;
+
+/**
+ * Echoes uppercase content of text frames.
+ */
+public class WebSocketFrameHandler extends SimpleChannelInboundHandler<WebSocketFrame> {
+//    private String sCode2SessionUrl = "https://api.weixin.qq.com/sns/jscode2session?appid=APPID&secret=SECRET&js_code=JSCODE&grant_type=authorization_code";
+//    private String appId = "wx4e23a5ec42c5a796";
+    private static float iCount = 0;
+
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        ctx.fireChannelActive();
+        iCount++;
+        System.out.println("channelActive"+iCount);
+    }
+
+    @Override
+    public void channelInactive(ChannelHandlerContext ctx) throws Exception {
+        ctx.fireChannelInactive();
+        iCount--;
+//        Redis.getInstance().close();
+        System.out.println("channelInactive"+iCount);
+    }
+
+    @Override
+    protected void channelRead0(ChannelHandlerContext ctx, WebSocketFrame frame) throws Exception {
+        if (frame instanceof TextWebSocketFrame) {
+            // Send the uppercase string back.
+            String request = ((TextWebSocketFrame) frame).text();
+            if (request.length() == 0)
+                return;
+            System.out.println(request);
+            int iColon = request.indexOf(":");
+            if (iColon == -1)
+                return;
+            String cmd = request.substring(0, iColon);
+            String sIdx = "";
+            String sName = "";
+            String sPass = "";
+            String sResponse = "200"; //请求成功
+            int i1 = request.indexOf("|", 1); //第一个"|"的位置；
+            if (i1 != -1)
+                sName = request.substring(iColon + 1, i1);
+            switch (cmd) {
+                case "getConnetedData":
+                    ctx.channel().writeAndFlush(new TextWebSocketFrame("getConnetedData" + "=" + iCount));
+                    break;
+                case "wxLogin":
+                    break;
+                case "register":
+                    sPass = request.substring(i1 + 1);
+                    boolean bSetName = Redis.getInstance().setName(sName, sPass);
+                    if (bSetName == false)
+                        sResponse = "名字已被注册";
+                    ctx.channel().writeAndFlush(new TextWebSocketFrame(cmd + ":" + sResponse));
+                    break;
+                case "login":
+                    sPass = request.substring(i1 + 1);
+                    String sPassTemp = Redis.getInstance().getName(sName);
+                    if (sPassTemp == null)
+                        sResponse = "用户名不存在";
+                    else if (sPassTemp.equals(sPass) == false)
+                        sResponse = "密码错误";
+                    ctx.channel().writeAndFlush(new TextWebSocketFrame(cmd + ":" + sResponse));
+                    break;
+                case "getScore":
+                    sName = request.substring(iColon + 1);
+                    ctx.channel().writeAndFlush(new TextWebSocketFrame(cmd + ":" + Redis.getInstance().getStrScore(sName)));
+                    break;
+                case "getStep":
+                    if (request.substring(iColon + 1).length() == 1)
+                        return;
+                    sIdx = request.substring(iColon + 1, iColon + 2);
+                    sName = request.substring(iColon + 2);
+                    if (sName == "")
+                        return;
+                    String sStep = Redis.getInstance().getStep(sIdx, sName);
+                    if (sStep != null)
+                        ctx.channel().writeAndFlush(new TextWebSocketFrame(cmd + ":" + sStep));
+                    break;
+                case "getRank":
+                    sIdx = request.substring(iColon + 1, iColon + 2);
+                    ctx.channel().writeAndFlush(new TextWebSocketFrame(cmd + ":" + Redis.getInstance().getStrRank(sIdx)));
+                    break;
+                case "setStep":
+                    i1 = request.indexOf("|", 1);
+                    if (i1 == -1)
+                        return;
+                    int i2 = request.indexOf("|", i1 + 1);
+                    if (i2 == -1)
+                        return;
+                    int i3 = request.indexOf("|", i2 + 1);
+                    if (i3 == -1)
+                        return;
+                    sIdx = request.substring(iColon + 1, iColon + 2);
+                    sName = request.substring(i1 + 1, i2);
+                    float iScore = Float.parseFloat(request.substring(i2 + 1, i3));
+                    sStep = request.substring(i3 + 1);
+                    Redis redis = Redis.getInstance();
+                    redis.addScore(sIdx, sName, iScore);
+                    redis.addStep(sIdx, sName, sStep);
+                    break;
+            }
+        } else {
+            String message = "unsupported frame type: " + frame.getClass().getName();
+            throw new UnsupportedOperationException(message);
+        }
+    }
+}
